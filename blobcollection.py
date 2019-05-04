24f058241e9d0a2cbd17ad567c4dab672cfe1578
Aaron <gonsalvesaaron99@gmail.com>
2019-05-04 17:22:47 +0100

    Unwanted attribute removed

--- blobcollection.py
+++ blobcollection.py
@@ -2,4 +2,4 @@ from google.appengine.ext import ndb
 
 class BlobCollection(ndb.Model):
 	blobs = ndb.BlobKeyProperty(repeated=True)
-	downloadblob = ndb.BlobKeyProperty();
+	# downloadblob = ndb.BlobKeyProperty();
